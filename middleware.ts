647d456198e9705ce5767ca351253351e0ea6ab2
BitMark Labs <markelorapti9@gmail.com>
2024-04-30 22:00:17 +0100

    five

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,8 @@
 import { authMiddleware } from "@clerk/nextjs/server";
 
 export default authMiddleware({
-  publicRoutes: ["/api/webhook", "/api/uploadthing"]
+  publicRoutes: ["/"],
+  debug: true
 });
 
 
