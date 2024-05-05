4d562243f46a762a8e57229158cc78a1b2392e1d
BitMark Labs <markelorapti9@gmail.com>
2024-05-05 16:32:43 +0100

    final

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,6 @@
 import { authMiddleware } from "@clerk/nextjs/server";
 
-export default authMiddleware ({
-  publicRoutes: ["/api/webhook", "/api/uploadthing", "/api/courses"],
-  debug: true
-});
+export default authMiddleware ();
 
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
